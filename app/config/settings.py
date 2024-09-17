cb26e51c0e5ca64557b5b610aca2d632acada340
FelipeHardmann <fashardmann@gmail.com>
2024-09-16 22:30:53 -0300

    :sparkles: feat: add sqlite

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,5 +5,5 @@ class Config:
     SQLACHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URL',
-        'mysql+mysqlconnector://root:Felipe2015#@localhost/turma'
+        'sqlite:///app.db'
     )
